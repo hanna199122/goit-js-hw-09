0dc5f387f3cd187c8dce8ac75e48f00c9badaf4f
Hanna <malahovaa79@gmail.com>
2023-04-24 16:56:35 +0300

    first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const bodyColor = document.querySelector('body');
+
+let timerId = null;
+
+btnStart.addEventListener('click', OnStartBtnClick);
+btnStop.addEventListener('click', onBtnStopClick);
+
+function OnStartBtnClick(evt) {
+  timerId = setInterval(() => {
+    console.log((bodyColor.style.backgroundColor = getRandomHexColor()));
+  }, 1000);
+  if (timerId) {
+    btnStart.disabled = true;
+  }
+}
+function onBtnStopClick(evt) {
+  clearInterval(timerId);
+  btnStart.disabled = false;
+}
